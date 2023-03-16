60f80483d622340a0dff6e1f04a4ec2b8dd8561b
S1aruW <florian.wagnergg@gmail.com>
2023-03-16 12:12:54 +0100

    add

---
+++ rpgRLjs.js
@@ -0,0 +1,15 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', refreshOrder);//make reload consistent
+
+function init(){
+  createCookies();
+}
+
+function createCookies(){
+  var benchpressCookie
+}
+
+function updateCookies(){
+  benchpressCookie = document.getElementById("benchpressInput").value;
+}
