98099cb8e87fe22c2c211233c8af54cefc77a0c3
S1aruW <florian.wagnergg@gmail.com>
2023-04-06 13:50:03 +0200

    as

--- rpgRLjs.js
+++ rpgRLjs.js
@@ -1,20 +1,6 @@
-const indexDB = 
-  window.indexedDB ||
-  window.mozIndexDB ||
-  window.msIndexDB ||
-  window.shIndexDB;
 
-  const request = indexDB.open("StatsDatabase",1);
-
-  request.onerror = function(event){
-    console.error("Error fak");
-    console.error(event);
-  }
-
-  request.onupgradeneeded = function(){
-    const db = request.result;
-    const store = db.createObjectStore("stats",{keyPath: "id"});
-    store.createIndex("strengthStats",["exercise", "weight"], {unique: false});
-  };
+function toggleInput(){
+  if(document.getElementsByClassName("input")
+}
 
   
